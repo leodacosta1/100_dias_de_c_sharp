23ea4223620885a97758cfc936762bc5aa5233d1
leodacosta1 <leonardotmcosta@gmail.com>
2024-09-05 17:18:25 -0300

    criei programa que soma 2 numeros na pasta do dia 2

---
+++ Dia_2/SomaApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um número: ");
+        int number1 = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Digite o segundo número: ");
+        int number2 = int.Parse(Console.ReadLine());
+
+        int result = number1 + number2;
+
+        Console.WriteLine("A soma dos números é: " + result);
+        
+    }
+}
