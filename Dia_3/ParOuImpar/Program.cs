1ed90f26555ce5d5aab7adb64ab2a93ef2223f01
leodacosta1 <leonardotmcosta@gmail.com>
2024-09-06 17:04:38 -0300

    criei app que verifica se numero e par e inicia dialogo do tela class usando if else

---
+++ Dia_3/ParOuImpar/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("É par ou ímpar? Digite um número inteiro com até 10 dígitos: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if(number % 2 == 0)
+        {
+            Console.WriteLine("É par!");
+        }
+        else
+        {
+            Console.WriteLine("É ímpar!");
+        }
+
+        if(number % 10 == 2)
+        {
+            Console.WriteLine("Qual o menu de hoje?");
+            string resposta = Console.ReadLine().Trim().ToLower(); // guarda a resposta do usuário em uma string para ser chamada posteriormente, se a condição for alcançada
+
+            if(resposta == "miojo") // agora usando a string que a gente criou pra guardar aquela resposta ali em cima
+            {
+                Console.WriteLine("Muito bem, velho. 2. vez 1. Diga!");
+                Console.WriteLine("Tu não é o fodão?");
+                string segundaResposta = Console.ReadLine().Trim().ToLower(); // guardando a segunda resposta agora, Trim() cortando espaços vazios e ToLower() deixando tudo caixa baixa
+                
+                if(segundaResposta == "2")
+                {
+                    Console.WriteLine("AAAAAAAAAAAAAAAAAAAAHHHHHHHH!");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Game Over");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Game Over");
+            }
+        }
+    }
+}
