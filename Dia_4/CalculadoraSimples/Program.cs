70d71faca4f9819c9f94d13a025e0233f18e2dc2
leodacosta1 <leonardotmcosta@gmail.com>
2024-09-07 18:32:08 -0300

    feita a primeira parte da calculadora simples em python

---
+++ Dia_4/CalculadoraSimples/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Posso realizar operações matemáticas simples, como: ");
+        Console.WriteLine("Soma");
+        Console.WriteLine("Subtração");
+        Console.WriteLine("Divisão");
+        Console.WriteLine("Multiplicação");
+        Console.WriteLine("Qual você quer que eu faça?");
+
+        string resposta = Console.ReadLine().Trim().ToLower(); // guarda a resposta do usuário em uma string para ser chamada posteriormente, se a condição for alcançada
+
+        switch(resposta == "Soma") // agora usando a string que a gente criou pra guardar aquela resposta ali em cima
+        {
+            case 1:
+                Console.WriteLine("Digite um número: ");
+                double numberSum1 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite um outro número: ");
+                double numberSum2 = double.Parse(Console.ReadLine());
+
+                double resultSum = numberSum1 + numberSum2;
+                Console.WriteLine("O Resultado é: " + resultSum);
+                break;
+            
+        }
+    }
+}
