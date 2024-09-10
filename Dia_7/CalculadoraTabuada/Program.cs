fd76299da570091e8b21a151c783d432d51d2a61
leodacosta1 <leonardotmcosta@gmail.com>
2024-09-10 16:37:04 -0300

    criei calculadora de tabuada

---
+++ Dia_7/CalculadoraTabuada/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um número: ");
+        int tabuada = int.Parse(Console.ReadLine());
+        int tabela = 0;
+
+        do
+        {
+            int resultado = tabuada * tabela;
+            Console.WriteLine($"{tabuada} x {tabela} = {resultado}");
+            tabela++;
+        } while (tabela <= 10);
+    }
+}
+
+
+
+    
