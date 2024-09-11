4bf2c536a356953dbfb462d90975cde0e5466e8d
leodacosta1 <leonardotmcosta@gmail.com>
2024-09-11 16:49:25 -0300

    criei calculadora simples que converte temperatura de celsius > farenheit

---
+++ Dia_8/ConversorCelsiusFarenheit/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a temperatura em Celsius: ");
+        float celsius = float.Parse(Console.ReadLine());
+
+        float farenheit = (celsius * 1.8f) + 32f;
+
+        Console.WriteLine($"A temperatura em Farenheit é {farenheit}");
+    }
+}
