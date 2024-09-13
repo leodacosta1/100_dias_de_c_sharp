184af511ba093276b541bff2dc9639e6b872e633
leodacosta1 <leonardotmcosta@gmail.com>
2024-09-12 22:43:22 -0300

    feita a lógica pro cálculo da média de um dataframe

---
+++ Dia_9/Dataframe/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] data = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
+
+        int sum = 0;
+
+        for (int counter = 0; counter < data.Length; counter++)
+        {
+            sum = sum + data[counter];
+        }
+        Console.WriteLine(sum);
+
+        int median = sum / data.Length;
+        
+        Console.WriteLine(median);
+    }
+}
