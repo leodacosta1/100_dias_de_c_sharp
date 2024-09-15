8aabd86e00e8444ed4ae0acc2fd8e3583f320514
leodacosta1 <leonardotmcosta@gmail.com>
2024-09-14 23:30:48 -0300

    criei projeto para não perder a estrutura de pastas e o hábito de comitar. Explicação no comentário do arquivo csharp do projeto criado pro dia 11

---
+++ Dia_11/volteDuasCasas/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("leia o comentário");
+
+// Volte para os dias 10 e 11 ou avance para o dia 21 quando ele tiver sido criado
