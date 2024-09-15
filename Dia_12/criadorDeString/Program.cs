cdee3e2e5f8d0db249cacdbfbbf20405ae5cf923
leodacosta1 <leonardotmcosta@gmail.com>
2024-09-15 19:35:57 -0300

    criei programa simples usando for loop pra criar nome das pastas para os dias restantes do desafio

---
+++ Dia_12/criadorDeString/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int dia = 0;
+
+        for (int i = 1; i <= 100; i++)
+        {
+            dia++;
+            Console.Write("Dia_" + dia + " ");
+        }
+    }
+}
+
+
