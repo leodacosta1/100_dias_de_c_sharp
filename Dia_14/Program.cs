3c041a3bffa7e9a653881319d253dcc0e02c271e
leodacosta1 <leonardotmcosta@gmail.com>
2024-09-17 23:35:22 -0300

    criei programa simples para extrair um valor máximo de um array usando namespace System.Linq pra executar o cálculo

---
+++ Dia_14/Program.cs
@@ -0,0 +1,14 @@
+﻿// Enunciado: encontre a média de salário entre os funcionários de TI
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] simpleArray = {100, 200, 300, 400, 500};
+        int highestValue = simpleArray.Max();
+        Console.WriteLine($"the highest value is {highestValue}");
+    }
+}
