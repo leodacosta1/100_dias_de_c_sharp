8a583b669820ce42455bccdacc04b63a177aa370
leodacosta1 <leonardotmcosta@gmail.com>
2024-09-19 23:52:11 -0300

    learned about random class, next function and created a random number generator

---
+++ Dia_16/RandomNumberGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random randomSomething = new Random(); // initialize the Random Class, creating the object of that class: Random()
+
+        int randomNumber = randomSomething.Next();
+
+        Console.WriteLine($"the random number is {randomNumber}");
+    }
+}
