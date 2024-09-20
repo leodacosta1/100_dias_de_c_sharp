a87d0ea6e7fb64644a22712cd1293dcaa07dc9ae
leodacosta1 <leonardotmcosta@gmail.com>
2024-09-19 23:53:31 -0300

    updated the comment explaining the Next() function

--- Dia_16/RandomNumberGenerator.cs
+++ Dia_16/RandomNumberGenerator.cs
@@ -6,7 +6,7 @@ class Program
     {
         Random randomSomething = new Random(); // initialize the Random Class, creating the object of that class: Random()
 
-        int randomNumber = randomSomething.Next();
+        int randomNumber = randomSomething.Next(); // Next() is a function part of the Random class, from the System namespace
 
         Console.WriteLine($"the random number is {randomNumber}");
     }
