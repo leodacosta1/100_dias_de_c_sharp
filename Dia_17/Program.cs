fc2b7fd1566b1b1bc6fa7c37f90f90461c37e133
leodacosta1 <leonardotmcosta@gmail.com>
2024-09-20 23:15:47 -0300

    feito programa que retorna uma senha aleatória com base em uma lista char[] de caracteres

---
+++ Dia_17/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random randomSomething = new Random();
+
+        char[] characterList = {'a', 'b', 'c', 'd', 'e', 'f', '1', '2', '3', '4', '5'};
+
+        int passwordLength = characterList.Length; // can also be assigned any int value, in case I want to change the password length
+
+        string password = "";
+        for (int counter = 0; counter < passwordLength; counter++)
+        {
+            int randomIndex = randomSomething.Next(characterList.Length);
+
+            password += characterList[randomIndex];
+        }
+
+        Console.WriteLine($"The new password is {password}");
+    }
+}
