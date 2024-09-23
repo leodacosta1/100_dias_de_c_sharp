7a5f46df54b2c51a27033c2696e43d242a49bbdc
leodacosta1 <leonardotmcosta@gmail.com>
2024-09-22 23:38:22 -0300

    completed the challenge to convert fahrenheit to celsius in the microsoft c# course. Revisiting basic concepts before taking more complex projects in with c#

---
+++ Dia_19/Program.cs
@@ -0,0 +1,15 @@
+﻿// Unit 5 of 8 - Perform basic operations on numbers in C# 
+//
+// Complete the challenge to convert Fahrenheit to Celsius
+//
+// To convert temperatures in degrees Fahrenheit to Celsius, first subtract 32, then multiply by five ninths (5 / 9).
+//
+// Display the result of the temperature conversion in a formatted message
+//
+// Combine the variables with literal strings passed into a series of Console.WriteLine() commands to form the complete message.
+
+
+int fahrenheit = 94;
+decimal celsius = (fahrenheit - 32m) * (5/9m);
+
+Console.WriteLine($"The temperature is: {celsius} Celsius");
