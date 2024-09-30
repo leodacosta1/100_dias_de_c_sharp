491e57064069b508a8ee0b6c7c1fff44ca3d3d2c
leodacosta1 <leonardotmcosta@gmail.com>
2024-09-29 22:22:42 -0300

    criei programa que soliciona enunciado do challenge em Add decision logic to your code using , , and  statements in C# com lógica levemente diferente do gabarito, mas que chega no resultado final

---
+++ Dia_26/ChallengeIfElse/Program.cs
@@ -0,0 +1,33 @@
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+Console.WriteLine(daysUntilExpiration);
+// Subscription expiration tracker
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine($"Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine($"Your subscription expires within a day!");
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days");
+}
+else
+{
+    Console.WriteLine($"Your subscription will expire soon. Renew now!");
+}
+
+// Discount logic
+if (daysUntilExpiration <= 5)
+{
+    discountPercentage = 10;
+    if (daysUntilExpiration == 1)
+    {
+        discountPercentage += 10;             
+    }    
+    Console.WriteLine($"Renew now and save {discountPercentage}%!");
+}
