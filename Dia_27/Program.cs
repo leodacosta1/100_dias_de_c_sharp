6eadbca3ffbcd35a399cfc5f1042ecfd7640b8d2
leodacosta1 <leonardotmcosta@gmail.com>
2024-09-30 23:25:53 -0300

    feita unidade 2 de 7 do módulo sobre como declarar e acessar arrays do curso de c# da microsoft

---
+++ Dia_27/Program.cs
@@ -0,0 +1,25 @@
+﻿// Create and run simple C# console applications (Get started with C#, Part 2)
+// Store and iterate through sequences of data using Arrays and the foreach statement in C#
+
+/*
+string[] fraudulentOrderIDs = new string[3];
+
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
+fraudulentOrderIDs[3] = "D000";
+*/
+
+string[] fraudulentOrderIDs = [ "A123", "B456", "C789" ];
+
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+fraudulentOrderIDs[0] = "F000";
+
+Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+// (!) Note: The Length property of an array is not zero-based.
+
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
