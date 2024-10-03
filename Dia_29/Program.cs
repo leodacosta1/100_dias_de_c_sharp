b7d4a2631206d16d116bc7eb49ec1c4745aa6afb
leodacosta1 <leonardotmcosta@gmail.com>
2024-10-02 21:48:29 -0300

    feito exercício sobre utilização de espaço vazio pra melhorar leitura do código

--- Dia_29/Program.cs
+++ Dia_29/Program.cs
@@ -51,6 +51,7 @@ It was a popular style years ago. However, most developers don't follow this adv
     consist of a letter from A to E, and a three
     digit number. Ex. A123.
 */
+/*
 Random random = new Random();
 string[] orderIDs = new string[5];
 
@@ -67,3 +68,39 @@ foreach (var orderID in orderIDs)
 {
     Console.WriteLine(orderID);
 }
+*/
+
+// ======================================================================== //
+// Unit 4 of 8 - Exercise - Use whitespace to make your code easier to read //
+// ======================================================================== //
+
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6; 
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
+}
+
+/*
+Use whitespace judiciously to improve the readability of your code.
+Use line feeds to create empty lines to separate phrases of code. A phrase includes lines of code that are similar, or work together.
+Use line feeds to separate code block symbols so that they are on their own line of code.
+Use the tab key to line up a code block with the keyword they're associated with.
+Indent code inside of a code block to show ownership.
+*/
