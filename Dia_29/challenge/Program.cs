d52e9e03b4a352dee1485a60c49100024e9223ee
leodacosta1 <leonardotmcosta@gmail.com>
2024-10-02 22:07:36 -0300

    terminei o challenge de limpar um pedaço de código renomeando variáveis, simplificando comentários e utilizando melhor o espaço em branco

---
+++ Dia_29/challenge/Program.cs
@@ -0,0 +1,54 @@
+﻿// Create and run simple C# console applications (Get started with C#, Part 2)
+// Create readable code with conventions, whitespace, and comments in C# 
+// learn.microsoft.com/
+
+// ================================================================================== //
+// Unit 5 of 8 - Exercise - Complete a challenge activity to improve code readability //
+// ================================================================================== //
+
+// Code challenge - apply style guidelines to improve readability
+
+/*
+string str = "The quick brown fox jumps over the lazy dog.";
+// convert the message into a char array
+char[] charMessage = str.ToCharArray();
+// Reverse the chars
+Array.Reverse(charMessage);
+int x = 0;
+// count the o's
+foreach (char i in charMessage) { if (i == 'o') { x++; } }
+// convert it back to a string
+string new_message = new string(charMessage);
+// print it out
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
+*/
+
+// ===================
+
+/*
+   This code reverses a message, counts the number of times 
+   a particular character appears, then prints the results
+   to the console window.
+ */
+
+string originalMessage = "The quick brown fox jumps over the lazy dog.";
+
+char[] convertedMessage = foxPhrase.ToCharArray();
+
+Array.Reverse(convertedMessage);
+
+int letterCount = 0;
+
+foreach (char counter in convertedMessage)
+{ 
+    if (counter == 'o')
+    { 
+        letterCount++;
+    }
+}
+
+string newMessage = new string(convertedMessage);
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {letterCount} times.");
