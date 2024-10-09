3b05c67399c70f941dbdcc4c19e4b4681529194b
leodacosta1 <leonardotmcosta@gmail.com>
2024-10-08 23:25:59 -0300

    feito exercício de operador condicional, unidade 3 de 9, path 3/6

---
+++ Dia_35/Program.cs
@@ -0,0 +1,20 @@
+﻿// learn.microsoft.com
+
+// Add logic to C# console applications (Get started with C#, Part 3) 
+// Evaluate Boolean expressions to make decisions in C# 
+
+// =========================================================== //
+// Unit 3 of 9 > Exercise - Implement the conditional operator //
+// =========================================================== //
+
+// <evaluate this condition> ? <if condition is true, return this value> : <if condition is false, return this value>
+
+
+int saleAmount = 1001;
+// saleAmount é maior que 1000? Se verdadeiro, retorne 100. Se falso, retorne 50.
+int discount = saleAmount > 1000 ? 100 : 50;
+Console.WriteLine($"Discount: {discount}");
+
+// versão simplificada, retirando a variável discount
+int saleAmount2 = 1001;
+Console.WriteLine($"Discount: {(saleAmount2 > 1000 ? 100 : 50)}");
