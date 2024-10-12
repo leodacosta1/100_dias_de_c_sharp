edec523c8c549802a78b68c7e24bc9fa9af747ed
leodacosta1 <leonardotmcosta@gmail.com>
2024-10-12 14:54:34 -0300

    feito exercício 2/6 do path 3/6 sobre switch statement do curso de c# da microsoft

---
+++ Dia_39/Program.cs
@@ -0,0 +1,32 @@
+﻿// learn.microsoft.com
+
+// Add logic to C# console applications (Get started with C#, Part 3) 
+// Branch the flow of code using the switch-case construct in C# 
+
+// ===================================================== //
+// Unit 2 of 7 > Exercise - Implement a switch statement //
+// ===================================================== //
+
+int employeeLevel = 100;
+string employeeName = "John Smith";
+
+string title = "";
+
+switch (employeeLevel)
+{
+    case 100:
+    case 200:
+        title = "Senior Associate";
+        break;
+    case 300:
+        title = "Manager";
+        break;
+    case 400:
+        title = "Senior Manager";
+        break;
+    default:
+        title = "Associate";
+        break;
+}
+
+Console.WriteLine($"{employeeName}, {title}");
