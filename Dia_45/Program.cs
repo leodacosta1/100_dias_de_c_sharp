09fe1f16c0fe7727dc87e8d0f1e9068233588db6
leodacosta1 <leonardotmcosta@gmail.com>
2024-10-18 23:56:48 -0300

    montei o code project 2 que valida string input. Tentando resolver bug com conversão de tipo null

---
+++ Dia_45/Program.cs
@@ -0,0 +1,31 @@
+﻿// learn.microsoft.com
+
+// Add logic to C# console applications (Get started with C#, Part 3) 
+// Add looping logic to your code using the do-while and while statements in C# 
+
+// ================================================================================================================= //
+// Unit 5 of 8 > Exercise - Complete a challenge activity to differentiate between do and while iteration statements //
+// ================================================================================================================= //
+
+// Code project 2 - write code that validates string input
+
+Console.WriteLine($"are you an Administrator, Manager or User?");
+
+bool userIsWrong = true;
+
+do
+{
+    string userInput = Console.ReadLine().Trim().ToLower();
+
+    if (userInput == "Administrator" || userInput == "Manager" || userInput == "User")
+    {
+        Console.WriteLine($"Welcome.");
+        break; //aqui é necessário o break ou o userIsWrong = true pra poder sair do loop caso tenha entrado no else uma vez
+    }
+    else
+    {
+        Console.WriteLine($"What?");
+        userIsWrong = false;
+    }
+    
+} while (userIsWrong == false);
