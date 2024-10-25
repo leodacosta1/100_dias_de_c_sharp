57e70c79426f4bb1fa045513180b4f13c9537f8a
leodacosta1 <leonardotmcosta@gmail.com>
2024-10-24 23:59:14 -0300

    criei programa simples em c# que retorna número de caracteres, inverte a frase e retorna frase separada por palavras em cada linha a partir de input do usuário

---
+++ Dia_51/Program.cs
@@ -0,0 +1,23 @@
+﻿
+        Console.Write("Digite uma frase: ");
+        string frase = Console.ReadLine();
+
+        
+        int numCaracteres = frase.Length;
+        Console.WriteLine($"Número de caracteres: {numCaracteres}");
+
+        
+        string fraseInvertida = "";
+        foreach (char c in frase)
+        {
+            fraseInvertida = c + fraseInvertida;
+        }
+        Console.WriteLine($"Frase invertida: {fraseInvertida}");
+
+
+        string[] palavras = frase.Split(' ');
+        Console.WriteLine("Palavras em linhas separadas:");
+        foreach (string palavra in palavras)
+        {
+            Console.WriteLine(palavra);
+        }
