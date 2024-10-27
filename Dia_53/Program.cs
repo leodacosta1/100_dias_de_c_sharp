1c3909500fea1321f4f836d353f8dbe9c94f4e6a
leodacosta1 <leonardotmcosta@gmail.com>
2024-10-26 23:41:25 -0300

    criei programa simples que calcula juros compostos a partir de input de usuário

---
+++ Dia_53/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite o valor principal: ");
+        float valorPrincipal = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a taxa de juros anual (%): ");
+        float taxaDeJuros = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a quantidade de anos: ");
+        int quantidadeDeAnos = int.Parse(Console.ReadLine());
+
+        float montante = CalculaJurosCompostos(valorPrincipal, taxaDeJuros, quantidadeDeAnos);
+
+        Console.WriteLine($"O montante após {quantidadeDeAnos} anos será: {montante:F2}");
+    }
+    static float CalculaJurosCompostos(float valorPrincipal, float taxaDeJuros, int quantidadeDeAnos)
+    {
+        float montante = valorPrincipal * (float)Math.Pow(1 + (taxaDeJuros / 100), quantidadeDeAnos);
+        return montante;
+    }
+}
