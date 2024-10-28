f72a3d479eacc59c91582d4d21d77d9ab3b1d942
leodacosta1 <leonardotmcosta@gmail.com>
2024-10-28 00:05:11 -0300

    criei contador de frases palavras e caracteres

---
+++ Dia_54/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite seu texto: ");
+        string texto = Console.ReadLine();
+
+        int palavras = texto.Split(new char[] { ' ', '\n', '\t' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        int frases = texto.Split(new char[] { '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        int caracteres = texto.Length;
+
+        Console.WriteLine($"Número de palavras: {palavras}");
+        Console.WriteLine($"Número de frases: {frases}");
+        Console.WriteLine($"Número de caracteres: {caracteres}");
+    }
+}
