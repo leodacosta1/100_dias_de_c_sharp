6d49194b516209d41f92c212f85708f0eb221505
leodacosta1 <leonardotmcosta@gmail.com>
2024-10-29 23:55:07 -0300

    criei programa que conta quantidade de vezes que a palavra inferno aparece no inferno de dante

---
+++ Dia_56/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string infernoText = "inferno.txt";
+        string inferno = "inferno";
+
+        string content = File.ReadAllText(infernoText);
+
+        int count = content.Split(new char[] { ' ', ',', '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries)
+                            .Count(word => word.Equals(inferno, StringComparison.OrdinalIgnoreCase));
+
+        Console.WriteLine($"The word '{inferno}' appears {count} times in the text.");
+    }
+}
