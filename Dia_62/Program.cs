cdfd311b78b461c93ca5082f6409ec23da686191
leodacosta1 <leonardotmcosta@gmail.com>
2024-11-04 15:29:47 -0300

    seguindo aula 'utilizando operadores' no balta.io do curso fundamentos do c#

---
+++ Dia_62/Program.cs
@@ -0,0 +1,22 @@
+﻿// Seguindo aula "Utilizando operadores" em bata.io
+// Fundamentos do C#
+
+using System;
+
+namespace MeyuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 2;
+            double y = 2;
+            x = (2 + 2) * 2;
+            y = 2 + (2 * 2);
+            bool z = x == y;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+        }
+    }
+}
