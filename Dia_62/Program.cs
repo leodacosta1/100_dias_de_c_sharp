de236e58cdd3ace6f59ee599f8adb1719da8c983
leodacosta1 <leonardotmcosta@gmail.com>
2024-11-05 17:33:29 -0300

    consertei erro de digitação em balta.io e adicionei programa sobre o uso de expressões booleanas dentro de parâmetro de loop if

--- Dia_62/Program.cs
+++ Dia_62/Program.cs
@@ -1,4 +1,4 @@
-﻿// Seguindo aula "Utilizando operadores" em bata.io
+﻿// Seguindo aula "Utilizando operadores" em balta.io
 // Fundamentos do C#
 
 using System;
@@ -9,6 +9,8 @@ namespace MeyuApp
     {
         static void Main(string[] args)
         {
+            // utilizamos o double a fim de obter resultados com números reais,
+            // caso haja, por exmeplo, uma divisão de 75 / 23.5
             double x = 2;
             double y = 2;
             x = (2 + 2) * 2;
---
+++ Dia_63/UtilizandoIf/Program.cs
@@ -0,0 +1,26 @@
+﻿// Seguindo aula "Utilizando If" em balta.io
+// Fundamentos do C#
+
+using System;
+
+namespace MeyuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 18;
+            int maioridade = 21;
+            int idadeMaxima = 65;
+
+            // pra usar uma expressão booleana com o operador de comparação, basta colocar
+            // a operação dentro de parênteses
+            if (!(idade <= maioridade))
+                Console.WriteLine("É diferente");
+            else
+                Console.WriteLine("É igual");
+            
+            Console.WriteLine("Finalizou o programa");
+        }
+    }
+}
