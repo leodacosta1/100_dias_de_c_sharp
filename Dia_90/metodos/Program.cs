83ea1ed83575d461630b72e00243ed791951c632
leodacosta1 <leonardotmcosta@gmail.com>
2024-12-03 20:48:21 -0300

    prática da aula métodos e funções do curso de c# do André Baltieri

---
+++ Dia_90/metodos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MeuApp;
+class Program
+{
+    static void Main(string[] args)
+    {
+        MeuMetodo();
+
+        string nome = RetornaNome("Leonardo", "Dacosta");
+        Console.WriteLine(nome);
+    }
+
+    static void MeuMetodo()
+    {
+        Console.WriteLine("C# é legal");
+    }
+
+    static string RetornaNome(
+        string nome,
+        string sobrenome,
+        int idade = 37, // uma forma de tornar o parâmetro idade como opcional é atribuir um valor diretamente na definição do método
+        bool teste = false,
+        double novo = 33.42
+    )
+    {
+        return nome + " " + sobrenome + " tem " + idade.ToString();
+    }
+}
