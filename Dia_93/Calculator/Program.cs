dc50118e0064f5ab5e791fc52266e8b8b9c258c8
leodacosta1 <leonardotmcosta@gmail.com>
2024-12-07 11:44:57 -0300

    fiz a primeira parte da Calculadora do curso Fundamentos do c# do balta.io e adicionei comentário explicando o uso do método .Parse() no Console.ReadLine().

---
+++ Dia_93/Calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿// Fundamentos do C# em balta.io
+// Mão na massa: Criando uma calculadora
+
+using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro valor: ");
+            // aqui, se não usarmos o .Parse(), o compilador acusa que não podemos fazer uma conversão implícita, já que o ReadLine() espera uma string. Estaríamos, portanto, sem o .Parse(), tentando converter implicitamente uma string para um float, a partir do momento em que declaramos que ela assumiria o valor da float var1.
+            float var1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(var1);
+        }
+    }
+}
