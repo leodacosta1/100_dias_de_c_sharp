cf6d4df18e31f9a5a0b3f3f1267661d3c9ba280e
leodacosta1 <leonardotmcosta@gmail.com>
2024-12-07 22:52:55 -0300

    adicionei função de soma na calculadora

--- Dia_93/Calculator/Program.cs
+++ Dia_93/Calculator/Program.cs
@@ -10,11 +10,17 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.Clear();
-            Console.WriteLine("Primeiro valor: ");
             // aqui, se não usarmos o .Parse(), o compilador acusa que não podemos fazer uma conversão implícita, já que o ReadLine() espera uma string. Estaríamos, portanto, sem o .Parse(), tentando converter implicitamente uma string para um float, a partir do momento em que declaramos que ela assumiria o valor da float var1.
+            Console.WriteLine("Primeiro valor: ");
             float var1 = float.Parse(Console.ReadLine());
 
-            Console.WriteLine(var1);
+            Console.WriteLine("Segundo valor:");
+            float var2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(""); // Esse comando é só pro Console pular uma linha
+
+            float resultado = var1 + var2;
+            Console.WriteLine($"O Resultado da soma é {resultado}");
         }
     }
 }
