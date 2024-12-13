a1287adc14639c8fd05af42b4177a25228f7395a
leodacosta1 <leonardotmcosta@gmail.com>
2024-12-13 10:41:09 -0300

    iniciei estrutura do cronometro com funcao start que carrega 2 variaveis e um loop while

---
+++ Dia_98/Stopwatch/Program.cs
@@ -0,0 +1,2 @@
+﻿// Fundamentos do C# em balta.io
+// Mão na massa: Cronômetro
---
+++ Dia_99/Stopwatch/Program.cs
@@ -0,0 +1,15 @@
+﻿// Fundamentos do C# em balta.io
+// Mão na massa: Cronômetro
+
+using System;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
