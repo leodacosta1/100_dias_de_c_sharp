7aa3fa7bce52cfe8930e300ce8fcf09be064d667
leodacosta1 <leonardotmcosta@gmail.com>
2024-12-13 11:53:45 -0300

    adicionei console.clear pra limpar os números iterados antes do atual e usei thread.sleep pra fazer a contagem dos números esperar 1s pra iterar pro próximo

--- Dia_99/Stopwatch/Program.cs
+++ Dia_99/Stopwatch/Program.cs
@@ -9,7 +9,25 @@ namespace Stopwatch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Start();
+        }
+
+        // inicia o cronômetro
+        static void Start()
+        {
+            int time = 10;
+            int currentTime = 0;
+
+            //exibe o temporizador
+            while (currentTime != time)
+            {
+                Console.Clear(); // aqui a gente limpa os números já contados para visualizarmos somente o número sendo contado no momento
+                currentTime++;
+                Console.WriteLine(currentTime); // aqui exibimos os números, ou melhor, o número atual, por conta do Console.Clear, mas o processador é muito rápido e não conseguimos visualizar a contagem. Por isso usamos o Thread.Sleep()
+
+                // Thread trabalha com a execução atual do processador. Vamos usar Thread para fazer o cronômetro esperar 1s antes de passar para o próximo número
+                Thread.Sleep(1000); // 1000 milissegundos
+            }
         }
     }
 }
